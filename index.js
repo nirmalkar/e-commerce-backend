6bf4bfb70d2b49fde4e0b1e3962cb809fd526739
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-22 03:20:39 +0530

    CONNECTED_DB: connected to database created route for user

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
-const app = express();
-app.use(cors({}));
-const data = require("./data");
 
+const app = express();
+
+const data = require("./data");
+app.use(cors({}));
+
+const userRoute = require("./routes/userRoute");
+dotenv.config();
+
+//connect to db
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB")
+);
+
+// middleware
+app.use(express.json());
+
+// routing middleware
+app.use("/api/users", userRoute);
+app.get("/", (req, res) => res.send("hello"));
 app.get("/api/products", (req, res) => res.send(data));
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
@@ -14,7 +35,7 @@ app.get("/api/products/:id", (req, res) => {
     res.status(400).send({ msg: "product not found" });
   }
 });
-
-app.listen(4000, () => {
-  console.log("app is running");
+const port = process.env.port || 4000;
+app.listen(port, () => {
+  console.log(`App is running at the port ${port}`);
 });
