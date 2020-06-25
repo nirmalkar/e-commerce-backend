9dd4c7792d6e54aed02b06af7a0df9d1b2dd6a95
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-26 02:59:45 +0530

    EDIT_&_DELETE: routes created for edit and delete

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const Product = require("../models/productModal");
 const { getToken } = require("../utils/index");
+const { findById } = require("../models/productModal");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const products = await product.find({});
-  res.send(products);
+  const allProducts = await Product.find({});
+  res.send(allProducts);
 });
 
 router.post("/", async (req, res) => {
@@ -27,6 +28,39 @@ router.post("/", async (req, res) => {
       .send({ message: "New product created", data: newProduct });
   } else {
     return res.status(500).send("Error in creating product");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findOne({ _id: productId });
+  if (product) {
+    product.name = req.body.name;
+    product.image = req.body.image;
+    product.brand = req.body.brand;
+    product.price = req.body.price;
+    product.category = req.body.category;
+    product.countInStock = req.body.countInStock;
+    product.description = req.body.description;
+  }
+  const updatedProduct = await product.save();
+  if (updatedProduct) {
+    return res
+      .status(200)
+      .send({ message: "Product Updated successfully", data: updatedProduct });
+  } else {
+    return res.status(500).send("Error in Updating product");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const productId = req.params.id;
+  const deletedProduct = await Product.findById(productId);
+  if (deletedProduct) {
+    deletedProduct.remove();
+    res.status(200).send("product deleted successfully");
+  } else {
+    res.send("Error in product deletion");
   }
 });
 
