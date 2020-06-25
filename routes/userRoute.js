c53088779539e6216c39f5a42d78fb3e7f762d0c
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-26 03:00:42 +0530

    REGISTER_USER: routes created for register user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/userModal");
-const getToken = require("../util");
+const { getToken } = require("../utils");
 
 router.post("/signin", async (req, res) => {
   const signInUser = await User.findOne({
@@ -14,10 +14,32 @@ router.post("/signin", async (req, res) => {
       name: signInUser.name,
       email: signInUser.email,
       isAdmin: signInUser.isAdmin,
-      token: getToken(signInUser),
+      token: await getToken(signInUser),
     });
   } else {
     res.status(401).send({ msg: "Invalid email or password" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    isAdmin: false,
+  });
+  const newUser = await user.save();
+
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data" });
   }
 });
 
